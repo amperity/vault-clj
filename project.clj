16f2a4a6d66f6cd3785f09f4b1700b330c8800c2
Greg Look <greglook@counsyl.com>
2015-10-27 14:48:52 -0700

    Add dependencies to project def.

--- project.clj
+++ project.clj
@@ -4,5 +4,9 @@
   :license {:name "Apache License"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
 
+  :deploy-branches ["master"]
+
   :dependencies
-  [[org.clojure/clojure "1.7.0"]])
+  [[clj-http "2.0.0"]
+   [org.clojure/clojure "1.7.0"]
+   [org.clojure/tools.logging "0.3.1"]])
