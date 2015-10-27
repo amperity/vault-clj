4d89d8db37f936bc3397de1266d79a176dbfa33f
Greg Look <greglook@counsyl.com>
2015-10-27 14:40:44 -0700

    Initial project skeleton.

---
+++ README.md
@@ -0,0 +1,12 @@
+vault-clj
+=========
+
+A Clojure library for interacting with the [Vault](https://vaultproject.io/)
+secret management system.
+
+## License
+
+Copyright © 2015 Counsyl, Inc.
+
+Distributed under the Apache License, Version 2.0. See the LICENSE file
+for more information.
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject counsyl/vault-clj "0.1.0-SNAPSHOT"
+  :description "Clojure client for the Vault secret management system."
+  :url "https://github.com/counsyl/vault-clj"
+  :license {:name "Apache License"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+
+  :dependencies
+  [[org.clojure/clojure "1.7.0"]])
