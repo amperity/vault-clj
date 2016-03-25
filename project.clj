c5cf1743dbd64bf92af7c85a3f1617ff13f1f888
Greg Look <greglook@counsyl.com>
2016-03-25 14:06:55 -0700

    Bump snapshot version, upgrade clj-http.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject counsyl/vault-clj "0.2.0-SNAPSHOT"
+(defproject counsyl/vault-clj "0.3.0-SNAPSHOT"
   :description "Clojure client for the Vault secret management system."
   :url "https://github.com/counsyl/vault-clj"
   :license {:name "Apache License"
@@ -9,6 +9,6 @@
 
   :dependencies
   [[cheshire "5.5.0"]
-   [clj-http "2.0.0"]
+   [clj-http "2.1.0"]
    [org.clojure/clojure "1.7.0"]
    [org.clojure/tools.logging "0.3.1"]])
