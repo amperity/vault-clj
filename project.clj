aa92ebd5d280ff85a1ef3ff51a31ae6f2eadf8ba
Greg Look <greglook@counsyl.com>
2016-03-25 13:57:27 -0700

    Set release version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject counsyl/vault-clj "0.2.0-SNAPSHOT"
+(defproject counsyl/vault-clj "0.2.0"
   :description "Clojure client for the Vault secret management system."
   :url "https://github.com/counsyl/vault-clj"
   :license {:name "Apache License"
