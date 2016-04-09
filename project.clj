332247055ea241b3c88e7efdf6a88573f457d534
Robin Walsh <rob.walsh@gmail.com>
2016-04-09 15:55:43 -0700

    Adding userpass login and ability to write secrets

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject counsyl/vault-clj "0.3.0-SNAPSHOT"
+(defproject counsyl/vault-clj "0.3.1-SNAPSHOT"
   :description "Clojure client for the Vault secret management system."
   :url "https://github.com/counsyl/vault-clj"
   :license {:name "Apache License"
