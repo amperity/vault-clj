4eff523f4046064f8e80f78f7223baad02e11059
Brett Lischalk (*) <blischalk@GR-028826MBP.local>
2018-03-09 22:15:24 -0600

    Adding app-role test

--- test/vault/client/http_test.clj
+++ test/vault/client/http_test.clj
@@ -2,7 +2,9 @@
   (:require
     [clojure.test :refer :all]
     [vault.core :as vault]
-    [vault.client.http :refer [http-client]]))
+    [vault.client.http :refer [http-client] :as h]))
+
+(def example-url "https://vault.example.com")
 
 
 (deftest http-client-instantiation
@@ -11,14 +13,34 @@
   (is (thrown? IllegalArgumentException
                (http-client :foo)))
   (is (instance? vault.client.http.HTTPClient
-                 (http-client "https://vault.example.com"))))
+                 (http-client example-url))))
 
 
 (deftest http-read-checks
-  (let [client (http-client "https://vault.example.com")]
+  (let [client (http-client example-url)]
     (is (thrown? IllegalArgumentException
                  (vault/read-secret client nil))
         "should throw an exception on non-string path")
     (is (thrown? IllegalStateException
                  (vault/read-secret client "secret/foo/bar"))
         "should throw an exception on unauthenticated client")))
+
+
+(deftest app-role
+  (let [api-endpoint (str example-url "/v1/auth/approle/login")
+        client (http-client example-url)
+        connection-attempt (atom nil)]
+
+    (with-redefs [h/do-api-request
+                  (fn [method url req]
+                    (reset! connection-attempt url))
+                  h/api-auth!
+                  (fn [claim auth-ref response] nil)]
+
+      (vault/authenticate! client
+                           :app-role
+                           {:secret-id "secret"
+                            :role-id "role-id"})
+
+      (is (= @connection-attempt api-endpoint)
+          (str "should attempt to auth with: " api-endpoint)))))
