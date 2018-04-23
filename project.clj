805524ca5238ffe0b0ba910676ca63211a4d47be
Greg Look <greg@greg-look.net>
2018-04-23 14:14:55 -0700

    Set release version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject amperity/vault-clj "0.5.2-SNAPSHOT"
+(defproject amperity/vault-clj "0.6.0"
   :description "Clojure client for the Vault secret management system."
   :url "https://github.com/amperity/vault-clj"
   :license {:name "Apache License"
