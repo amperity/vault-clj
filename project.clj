eacde9d69297ce5c5e5eb98d7f6d5ee5295b48b2
Greg Look <greg@greg-look.net>
2018-04-23 14:20:11 -0700

    Upgrade dependencies.

--- project.clj
+++ project.clj
@@ -8,10 +8,10 @@
   :pedantic? :abort
 
   :dependencies
-  [[org.clojure/clojure "1.8.0"]
-   [org.clojure/tools.logging "0.3.1"]
+  [[org.clojure/clojure "1.9.0"]
+   [org.clojure/tools.logging "0.4.0"]
    [amperity/envoy "0.3.1"]
-   [cheshire "5.7.1"]
+   [cheshire "5.8.0"]
    [clj-http "2.3.0"]
    [com.stuartsierra/component "0.3.2"]]
 
