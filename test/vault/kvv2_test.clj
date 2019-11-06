ca921e0432b846e9e527263a043e94438bab0d8a
Daniel Rassaby <drassaby@gmail.com>
2019-11-06 15:06:51 -0800

    Added tests for kvv2 config read/write

--- test/vault/kvv2_test.clj
+++ test/vault/kvv2_test.clj
@@ -6,7 +6,43 @@
       ExceptionInfo)))
 
 
-(deftest read
+(deftest write-config-test
+  (let [mount "mount"
+        token-passed-in "fake-token"
+        vault-url "https://vault.example.amperity.com"
+        client (vault.core/new-client vault-url)
+        new-config {:max_versions 5
+                    :cas_require false
+                    :delete_version_after}]
+    (testing "Config can be updated with valid call"
+      (with-redefs
+        [clj-http.client/post
+         (fn [url opts]
+           (is (= (str vault-url "/v1/" mount "/config") url))
+           (is (= token-passed-in (get (:headers opts) "X-Vault-Token")))
+           (is (= new-config (:data opts))))]
+        (vault-kv/write-config client mount new-config)))))
+
+
+(deftest read-config-test
+  (let [config {:max_versions 5
+                :cas_require false
+                :delete_version_after}
+        mount "mount"
+        token-passed-in "fake-token"
+        vault-url "https://vault.example.amperity.com"
+        client (vault.core/new-client vault-url)]
+    (testing "Config can be read with valid call"
+      (with-redefs
+        [clj-http.client/get
+         (fn [url opts]
+           (is (= (str vault-url "/v1/" mount "/config") url))
+           (is (= token-passed-in (get (:headers opts) "X-Vault-Token")))
+           {:data config})]
+        (is (= config (vault-kv/read-config client mount)))))))
+
+
+(deftest read-test
   (let [lookup-response-valid-path {:data {:data     {:foo "bar"}
                                            :metadata {:created_time  "2018-03-22T02:24:06.945319214Z"
                                                       :deletion_time ""
