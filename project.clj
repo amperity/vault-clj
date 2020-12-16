11a2ad60c5e9ff727e9a2b432aea61dced788f92
Greg Look <greg@mvxcvi.com>
2020-12-16 10:48:02 -0800

    Update dependencies.

--- project.clj
+++ project.clj
@@ -8,12 +8,12 @@
   :pedantic? :abort
 
   :dependencies
-  [[org.clojure/clojure "1.10.0"]
-   [org.clojure/tools.logging "0.4.0"]
+  [[org.clojure/clojure "1.10.1"]
+   [org.clojure/tools.logging "1.1.0"]
    [amperity/envoy "0.3.3"]
-   [cheshire "5.8.1"]
-   [clj-http "3.7.0"]
-   [com.stuartsierra/component "0.4.0"]]
+   [cheshire "5.10.0"]
+   [clj-http "3.11.0"]
+   [com.stuartsierra/component "1.0.0"]]
 
   :codox
   {:metadata {:doc/format :markdown}
@@ -30,12 +30,12 @@
 
    :repl
    {:source-paths ["dev"]
-    :dependencies [[org.clojure/tools.namespace "0.3.0"]]
+    :dependencies [[org.clojure/tools.namespace "1.1.0"]]
     :jvm-opts ["-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog"
                "-Dorg.apache.commons.logging.simplelog.showdatetime=true"
                "-Dorg.apache.commons.logging.simplelog.defaultlog=info"
                "-Dorg.apache.commons.logging.simplelog.log.vault=debug"]}
 
    :test
-   {:plugins [[lein-cloverage "1.0.9"]]
+   {:plugins [[lein-cloverage "1.2.1"]]
     :jvm-opts ["-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.NoOpLog"]}})
