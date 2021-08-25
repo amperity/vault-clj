fc81cd7e33baad56812e00b2580d100e90240486
Colin Lappala <colin@amperity.com>
2021-08-25 11:00:23 -0700

    bump snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject amperity/vault-clj "1.0.6"
+(defproject amperity/vault-clj "1.0.7-SNAPSHOT"
   :description "Clojure client for the Vault secret management system."
   :url "https://github.com/amperity/vault-clj"
   :license {:name "Apache License"
