3f854f0a059f5622d57ab45510514d5b3c2cf3b4
Greg Look <greg@mvxcvi.com>
2021-09-10 10:12:46 -0700

    Change project group to com.amperity.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject amperity/vault-clj "2.0.0-SNAPSHOT"
+(defproject com.amperity/vault-clj "2.0.0-SNAPSHOT"
   :description "Clojure client for the Vault secret management system."
   :url "https://github.com/amperity/vault-clj"
   :license {:name "Apache License"
