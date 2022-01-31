73061053a99805155303ddee5092035f0618de7c
Greg Look <greg@mvxcvi.com>
2022-01-31 12:25:38 -0800

    Bump SNAPSHOT version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject amperity/vault-clj "1.1.1"
+(defproject amperity/vault-clj "1.1.2-SNAPSHOT"
   :description "Clojure client for the Vault secret management system."
   :url "https://github.com/amperity/vault-clj"
   :license {:name "Apache License"
