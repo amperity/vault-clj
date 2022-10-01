75736424a4a4f4bea0d662c57471a3c199e68158
brendonjwong <brendon@amperity.com>
2022-10-01 12:28:07 -0700

    Add assertions that `vault/auth-info` returns an authenticated map.

--- test/vault/auth/userpass_test.clj
+++ test/vault/auth/userpass_test.clj
@@ -1,8 +1,17 @@
 (ns vault.auth.userpass-test
   (:require
     [clojure.test :refer [deftest is testing]]
+    [vault.auth :as auth]
     [vault.auth.userpass :as userpass]
+    [vault.client :as vault]
     [vault.integration :refer [with-dev-server cli]]))
+
+
+(defn- assert-authenticated-map
+  [auth]
+  (is (string? (:accessor auth)))
+  (is (string? (:client-token auth)))
+  (is (pos-int? (:lease-duration auth))))
 
 
 (deftest ^:integration http-api
@@ -11,15 +20,23 @@
       (testing "with default mount"
         (cli "auth" "enable" "userpass")
         (cli "write" "auth/userpass/users/foo" "password=bar")
-        (let [auth (userpass/login client "foo" "bar")]
-          (is (string? (:accessor auth)))
-          (is (string? (:client-token auth)))
-          (is (pos-int? (:lease-duration auth)))))
+        (let [original-auth-info (vault/auth-info client)
+              response (userpass/login client "foo" "bar")
+              auth-info (vault/auth-info client)]
+          (assert-authenticated-map response)
+          (is (= (:client-token response)
+                 (::auth/client-token auth-info)))
+          (is (not= (::auth/client-token original-auth-info)
+                    (::auth/client-token auth-info)))))
       (testing "with alternate mount"
         (cli "auth" "enable" "-path=auth-test" "userpass")
         (cli "write" "auth/auth-test/users/baz" "password=qux")
-        (let [client' (userpass/with-mount client "auth-test")
-              auth (userpass/login client' "baz" "qux")]
-          (is (string? (:accessor auth)))
-          (is (string? (:client-token auth)))
-          (is (pos-int? (:lease-duration auth))))))))
+        (let [original-auth-info (vault/auth-info client)
+              client' (userpass/with-mount client "auth-test")
+              response (userpass/login client' "baz" "qux")
+              auth-info (vault/auth-info client)]
+          (assert-authenticated-map response)
+          (is (= (:client-token response)
+                 (::auth/client-token (vault/auth-info client))))
+          (is (not= (::auth/client-token original-auth-info)
+                    (::auth/client-token auth-info))))))))
